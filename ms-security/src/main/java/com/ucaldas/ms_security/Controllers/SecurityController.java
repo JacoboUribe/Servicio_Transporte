d8662be99c5c46ebcb14745d4afd2ac1d0ce5217
JacoboUribe <jacobo.uribe36921@ucaldas.edu.co>
2024-10-22 22:25:41 -0500

    cambios 22/10/24

--- ms-security/src/main/java/com/ucaldas/ms_security/Controllers/SecurityController.java
+++ ms-security/src/main/java/com/ucaldas/ms_security/Controllers/SecurityController.java
@@ -12,6 +12,7 @@ import com.ucaldas.ms_security.Services.JwtService;
 // import com.ucaldas.ms_security.Services.NotificationsService;
 import com.ucaldas.ms_security.Services.ValidatorsService;
 
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,8 +35,7 @@ public class SecurityController {
     private EncryptionService theEncryptionService;
     @Autowired
     private JwtService theJwtService;
-    // @Autowired
-    // private NotificationsService theNotificationsService;
+
     @Autowired
     private ValidatorsService theValidatorsService;
 
@@ -58,74 +58,5 @@ public class SecurityController {
         }
     }
 
-   @PostMapping("login/2FA/{idUser}")
-    public String secondFactor(@RequestBody Session theNewSession, @PathVariable String idUser, final HttpServletResponse response) throws IOException {
-
-        User theUser = this.theUserRepository.findById(idUser).orElse(null);
-        Session theOldSession = this.theSessionRepository.getSession(theUser.get_id(), theNewSession.getCode2fa());
-
-        if (theUser != null && theOldSession != null) {
-            if (theNewSession.getCode2fa().equals(theOldSession.getCode2fa()) && !theOldSession.isActive()) {
-                String token = theJwtService.generateToken(theUser);
-                theOldSession.setActive(true);  
-                this.theSessionRepository.save(theOldSession);
-                response.setStatus(HttpServletResponse.SC_OK);
-                return "message: "+ token;
-            } else {
-                response.sendError(HttpServletResponse.SC_UNAUTHORIZED); 
-            }
-        }
-
-        return null;
-    }
-
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    @DeleteMapping("/singout/session/{idSession}")
-    public void closeSession(@PathVariable String idUser, @PathVariable String idSession, final HttpServletResponse response)throws IOException {
-        Session theSession = this.theSessionRepository.findById(idSession).orElse(null);
-        if (theSession != null) {
-            this.theSessionRepository.delete(theSession);
-        }
-    }
-    // @PostMapping("/resetpassword/{userId}")
-    // public String resetPassword(@PathVariable String userId, final HttpServletResponse response) throws IOException{
-    //     User theActualUser = this.theUserRepository.findById(userId).orElse(null);
-    //         if (theActualUser != null){
-    //             String number = this.generateRandom();
-    //             theActualUser.setResetCode(number);
-    //             this.theUserRepository.save(theActualUser);
-    //             theNotificationsService.sendResetLink(theActualUser, number);
-    //         }else{
-    //             response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
-    //             return "message: User not found";
-    //         }
-    //     return "message: Reset code sent";
-
-    // }
-        
-
-    @PostMapping("/resetpassword/{userId}/{code}")
-    public String resetPassword(@PathVariable String userId , @PathVariable String code, @RequestBody String password, final HttpServletResponse response) throws IOException{
-        User theActualUser = this.theUserRepository.findById(userId).orElse(null);
-            if (theActualUser.getResetcode().equals(code)){
-                theActualUser.setPassword(theEncryptionService.convertSHA256(password));
-                theActualUser.setResetCode("");
-                this.theUserRepository.save(theActualUser);
-                return "message: Password reseted";
-            }
-        return "message: algo salio mal";
-        
-    }
-
-    @PostMapping("permissions-validation")
-    public boolean permissionsValidation(final HttpServletRequest request, @RequestBody Permission thePermission){
-        boolean succes = this.theValidatorsService.validationRolePermission(request, thePermission.getUrl(), thePermission.getMethod());
-        return succes;
-    }
-
-    public String generateRandom() {
-        int number = (int)(Math.random()*90000+10000);
-        return String.valueOf(number);
-    }
 
 }
--- ms-security/src/main/java/com/ucaldas/ms_security/MsSecurityApplication.java
+++ ms-security/src/main/java/com/ucaldas/ms_security/MsSecurityApplication.java
@@ -3,7 +3,7 @@ package com.ucaldas.ms_security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication	
 public class MsSecurityApplication {
 
 	public static void main(String[] args) {
